a2ccc9d1bdb83afd57fdd4dbfd61e449bfc2ad9b
thatsimplekid <thatsimplekid@gmail.com>
2013-09-08 14:48:06 +0100

    Added .gitignore
    Now our eclipse settings won't sync, keep them personal

---
+++ forge/mcp/src/minecraft/com/polegamers/kitchencraft/BlockTile.java
@@ -0,0 +1,13 @@
+package com.polegamers.kitchencraft;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockTile extends Block {
+
+	public BlockTile(int par1, "") {
+		super(par1, par2Material);
+		// TODO Auto-generated constructor stub
+	}
+
+}
