1f257420a1816aa811b6204f0ebc3fd75afc5129
thatsimplekid <thatsimplekid@gmail.com>
2013-09-15 11:09:57 +0100

    Added Knife - Updated res

--- forge/mcp/src/minecraft/com/polegamers/kitchencraft/BlockTile.java
+++ forge/mcp/src/minecraft/com/polegamers/kitchencraft/BlockTile.java
@@ -1,13 +1,36 @@
 package com.polegamers.kitchencraft;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.util.Icon;
 
 public class BlockTile extends Block {
 
-	public BlockTile(int par1, "") {
-		super(par1, par2Material);
-		// TODO Auto-generated constructor stub
+	public BlockTile(int par1, String par2) {
+		super(par1, Material.rock);
+		this.setResistance(1F);
+		this.setHardness(1F);
+		this.setUnlocalizedName(par2 + " Tiles");
+		this.setCreativeTab(kcbasemod.kcTab);
+		this.blockIcon = this.black;
 	}
+	
+	public Icon black;
+	public Icon red;
+	public Icon green;
+	public Icon blue;
+	
+	@Override
+	public void registerIcons(IconRegister par1IconRegister)
+    {
+        this.blockIcon = par1IconRegister.registerIcon("kc/" + this.getUnlocalizedName());
+        this.black = par1IconRegister.registerIcon("kc/black");
+        this.red = par1IconRegister.registerIcon("kc/red");
+        this.green = par1IconRegister.registerIcon("kc/green");
+        this.blue = par1IconRegister.registerIcon("kc/blue");
+    }
 
 }
--- forge/mcp/src/minecraft/com/polegamers/kitchencraft/kcbasemod.java
+++ forge/mcp/src/minecraft/com/polegamers/kitchencraft/kcbasemod.java
@@ -1,10 +1,14 @@
 package com.polegamers.kitchencraft;
 
+import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 import net.minecraft.src.BaseMod;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
 
 @Mod(modid="kitchencraft", name="KitchenCraft", version="pre-alpha v0.1")
 @NetworkMod(clientSideRequired = true, serverSideRequired = true)
@@ -12,9 +16,12 @@ import cpw.mods.fml.common.network.NetworkMod;
 public class kcbasemod extends BaseMod{
 	
 	public static CreativeTabs kcTab = new CreativeTabKitchenCraft("kctab");
+	
+	public static Block blackTiles = new BlockTile(508,"Black");
+	
+	public static Item itemKnife = new ItemKnife(1050);
 
-
-
+	
 @Override
 public String getVersion() {
 	return "pre-alpha v0.1";
@@ -24,7 +31,8 @@ public String getVersion() {
 @Init
 public void load(){
 	
-	
+	GameRegistry.registerBlock(blackTiles, "BlackTiles");
+	LanguageRegistry.addName(blackTiles, "Black Tiles");
 
 }
 
