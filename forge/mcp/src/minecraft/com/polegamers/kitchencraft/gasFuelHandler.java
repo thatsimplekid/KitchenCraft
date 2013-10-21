4ae5ce177048dd9c05eecc18fb4888aa94e53c7d
Anteater46 <anteater46@polegamers.com>
2013-10-21 17:29:51 +0100

    Added Fuels & Handler
    It may bug again, but the recoded basemod and fuelhandler should appear
    now.

---
+++ forge/mcp/src/minecraft/com/polegamers/kitchencraft/gasFuelHandler.java
@@ -0,0 +1,18 @@
+package com.polegamers.kitchencraft;
+
+import net.minecraft.item.ItemStack;
+import cpw.mods.fml.common.IFuelHandler;
+
+public class gasFuelHandler implements IFuelHandler{
+
+	@Override
+	public int getBurnTime(ItemStack fuel) {
+		if(fuel.itemID == kcbasemod.smallGasCan.itemID){
+			return 500;
+		}else{
+			return 0;
+		
+		}
+	}
+
+}
